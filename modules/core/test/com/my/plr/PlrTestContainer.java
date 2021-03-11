6eb23552760e6a60eb52ab1ae64256b4a3a14c3c
Andrew <kardinalmail@gmail.com>
2021-03-11 14:00:18 +0300

    started, models

---
+++ modules/core/test/com/my/plr/PlrTestContainer.java
@@ -0,0 +1,58 @@
+package com.my.plr;
+
+import com.haulmont.cuba.testsupport.TestContainer;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class PlrTestContainer extends TestContainer {
+
+    public PlrTestContainer() {
+        super();
+        //noinspection ArraysAsListWithZeroOrOneArgument
+        appComponents = new ArrayList<>(Arrays.asList(
+                // list add-ons here: "com.haulmont.reports", "com.haulmont.addon.bproc", etc.
+                "com.haulmont.cuba"
+        ));
+        appPropertiesFiles = Arrays.asList(
+                // List the files defined in your web.xml
+                // in appPropertiesConfig context parameter of the core module
+                "com/my/plr/app.properties",
+                // Add this file which is located in CUBA and defines some properties
+                // specifically for test environment. You can replace it with your own
+                // or add another one in the end.
+                "com/my/plr/test-app.properties");
+        autoConfigureDataSource();
+    }
+
+    
+
+    public static class Common extends PlrTestContainer {
+
+        public static final PlrTestContainer.Common INSTANCE = new PlrTestContainer.Common();
+
+        private static volatile boolean initialized;
+
+        private Common() {
+        }
+
+        @Override
+        public void beforeAll(ExtensionContext extensionContext) throws Exception {
+            if (!initialized) {
+                super.beforeAll(extensionContext);
+                initialized = true;
+            }
+            setupContext();
+        }
+        
+
+        @SuppressWarnings("RedundantThrows")
+        @Override
+        public void afterAll(ExtensionContext extensionContext) throws Exception {
+            cleanupContext();
+            // never stops - do not call super
+        }
+        
+    }
+}
---
+++ modules/global/src/com/my/plr/entity/Speaker.java
@@ -0,0 +1,58 @@
+package com.my.plr.entity;
+
+import com.haulmont.chile.core.annotations.NamePattern;
+import com.haulmont.cuba.core.entity.StandardEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+@Table(name = "PLR_SPEAKER", indexes = {
+        @Index(name = "IDX_PLR_SPEAKER_LASTNAME", columnList = "LAST_NAME")
+})
+@Entity(name = "plr_Speaker")
+@NamePattern("%s %s|firstName,lastName")
+public class Speaker extends StandardEntity {
+
+    private static final long serialVersionUID = 613431938653322478L;
+
+    @NotNull
+    @Column(name = "FIRST_NAME", nullable = false)
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @NotNull
+    @Column(name = "EMAIL", nullable = false, unique = true)
+    @Email
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+}

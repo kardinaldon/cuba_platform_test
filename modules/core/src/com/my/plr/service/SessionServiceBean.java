5155d565a076ad7361e74598b520e6c3cdb53429
Andrew <kardinalmail@gmail.com>
2021-03-13 22:53:57 +0300

    fix report editor

---
+++ modules/core/src/com/my/plr/service/SessionServiceBean.java
@@ -0,0 +1,38 @@
+package com.my.plr.service;
+
+import com.my.plr.entity.Session;
+import org.springframework.stereotype.Service;
+import com.haulmont.cuba.core.global.DataManager;
+import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+@Service(SessionService.NAME)
+public class SessionServiceBean implements SessionService {
+    @Inject
+    private DataManager dataManager;
+
+    @Override
+    public Session rescheduleSession(Session session, LocalDateTime newStartDate) {
+
+        LocalDateTime dayStart = newStartDate.truncatedTo(ChronoUnit.DAYS).withHour(8);
+        LocalDateTime dayEnd = newStartDate.truncatedTo(ChronoUnit.DAYS).withHour(19);
+
+        Long sessionsSameTime = dataManager.loadValue("select count(s) from sessionplanner_Session s " +
+                "where (s.startDate between :dayStart and :dayEnd) " +
+                "and s.speaker = :speaker " +
+                "and s.id <> :sessionId", Long.class)
+                .parameter("dayStart", dayStart)
+                .parameter("dayEnd", dayEnd)
+                .parameter("speaker", session.getSpeaker())
+                .parameter("sessionId", session.getId())
+                .one();
+        if (sessionsSameTime >= 2) {
+            return session;
+        }
+        session.setStartDate(newStartDate);
+        return dataManager.commit(session);
+    }
+}
--- modules/global/src/com/my/plr/service/SessionService.java
+++ modules/global/src/com/my/plr/service/SessionService.java
@@ -4,6 +4,6 @@ import com.my.plr.entity.Session;
 import java.time.LocalDateTime;
 
 public interface SessionService {
-    String NAME = "sessionplanner_SessionService";
+    String NAME = "plr_SessionService";
     Session rescheduleSession(Session session, LocalDateTime newStartDate);
 }
